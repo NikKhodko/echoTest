e9b77176f2c297eb249871b5d72449b07baf2b04
NikKhodko <nik.khodko@mail.ru>
2024-07-24 15:08:05 +0500

    1stcom

---
+++ main.go
@@ -0,0 +1,116 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+type User struct {
+	Name string `json:"name"`
+	Type string `json:"type"`
+}
+
+type Admin struct {
+	Name string `json:"name"`
+	Type string `json:"type"`
+}
+
+type Boss struct {
+	Name string `json:"name"`
+	Type string `json:"type"`
+}
+
+func hola(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello, gay")
+}
+func addUser(c echo.Context) error {
+	user := User{}
+
+	defer c.Request().Body.Close()
+
+	b, err := io.ReadAll((c.Request().Body))
+
+	if err != nil {
+		log.Printf("failed reading the request body: %s", err)
+		return c.String(http.StatusInternalServerError, "")
+	}
+
+	err = json.Unmarshal(b, &user)
+	if err != nil {
+		log.Printf("failed unmarshalling in addUsers: %s\n", err)
+		return c.String(http.StatusInternalServerError, "")
+	}
+
+	log.Printf("this is user: %#v\n", user)
+	return c.String(http.StatusOK, "we got user")
+}
+
+func addAdmin(c echo.Context) error {
+	admin := Admin{}
+
+	defer c.Request().Body.Close()
+
+	err := json.NewDecoder(c.Request().Body).Decode(&admin)
+	if err != nil {
+		log.Printf("failed proccesing addAdmin request: %s/n", err)
+		return echo.NewHTTPError(http.StatusInternalServerError)
+	}
+
+	log.Printf("this is admin: %#v", admin)
+	return c.String(http.StatusOK, "we got admin")
+}
+
+func addBoss(c echo.Context) error {
+	boss := Boss{}
+
+	err := c.Bind(&boss)
+	if err != nil {
+		log.Printf("failed proccessing addBoss request:")
+		return echo.NewHTTPError(http.StatusInternalServerError)
+	}
+
+	log.Printf("this is boss: %#v", boss)
+	return c.String(http.StatusOK, "we got boss")
+}
+
+func getUser(c echo.Context) error {
+
+	userName := c.QueryParam("name")
+	userType := c.QueryParam("type")
+	dataType := c.Param("data")
+
+	if dataType == "string" {
+		return c.String(http.StatusOK, fmt.Sprintf("user name is: %s\n his type is: %s\n", userName, userType))
+	}
+
+	if dataType == "json" {
+		return c.JSON(http.StatusOK, map[string]string{
+			"name": userName,
+			"type": userType,
+		})
+	}
+
+	return c.JSON(http.StatusBadRequest, map[string]string{
+		"error": "lets try again. string or json",
+	})
+}
+
+func main() {
+	fmt.Println("welcome to the server")
+
+	e := echo.New()
+
+	e.GET("/", hola)
+	e.GET("/users/:data", getUser)
+
+	e.POST("/users", addUser)
+	e.POST("/admins", addAdmin)
+	e.POST("/boss", addBoss)
+
+	e.Logger.Fatal(e.Start(":1333"))
+}
